31c27c7c4bdecd8188b94338a1206dc1c380019b
rbgir1113-dot <rbgir1113@gmail.com>
2026-02-11 23:17:54 +0900

    feat: day03

--- control/src/whileTask/WhileTask02.java
+++ control/src/whileTask/WhileTask02.java
@@ -18,6 +18,25 @@ import java.util.Scanner;
 public class WhileTask02 {
    public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      
+      String message = "정수 값을 입력하세요\n누적 합이 100이상 되면 종료된다.",
+    		  inputValues = "" ;
+      int inputValue = 0, totalValue = 0;
+      while(true) {
+    	  System.out.println(message);
+    	  inputValue = sc.nextInt();
+    	  totalValue += inputValue;
+    	  System.out.println(totalValue);
+    	  if(inputValues.equals("")) {
+    		  inputValues = "" + inputValue;
+    	  }else {
+    		  inputValues = inputValues + " , " + inputValue;
+    	  }
+    	  if(totalValue >= 100) {
+    		  System.out.println("누적합 100이상이므로 프로그램 종료");
+    		  break;
+    	  }
+      }
+      System.out.println("입력한 값 : " + inputValues);
+      System.out.println("총 결과 값 : " + totalValue);
    }
 }
