2760c005e2c733c88b6dd8d9cc56f41006d16054
rbgir1113-dot <rbgir1113@gmail.com>
2026-02-22 23:45:47 +0900

    feat: practice day

--- practice/src/practiceClass/PracticeClass.java
+++ practice/src/practiceClass/PracticeClass.java
@@ -1,7 +1,72 @@
 package practiceClass;
 
+import practiceClass.Bank;
+import practiceClass.Person;
+
+//Person, Bank
+
+// Bank 클래스 생성
+class Bank{
+	String bankName;
+	long bankMoney;
+
+	
+//	Bank 기본 생성자
+	public Bank() {;}
+	
+//	Bank 초기화 생성자
+	public Bank(String bankName) { // money를 미리 입력하지 않을것이므로 money는 배제
+		this.bankName = bankName;
+	}
+
+	
+//	사용자의 돈을 입금하는 메서드	
+//	수수료 1000원
+	void deposit(int money, Person person) {
+		this.bankMoney += money + 1000;
+		person.personMoney -= money + 1000;
+	}
+	
+//	사용자의 돈을 출금하는 메서드
+//	수수료 5000원
+	void withdraw(int money, Person person) {
+		this.bankMoney -= money - 5000;
+		person.personMoney += money - 5000;
+	}
+	
+	
+	
+}
+
+// Person 클래스 생성
+class Person{
+	String personName;
+	int personMoney;
+
+//	Person 기본 생성자
+	public Person() {;}
+//	Person 초기화 생성자
+	public Person(String personName, int personMoney) {
+		this.personName = personName;
+		this.personMoney = personMoney;
+	}
+	
+	
+}
+
+
 public class PracticeClass {
 	public static void main(String[] args) {
+		Bank kb = new Bank("국민은행");
+		Person gildong = new Person();
+		
+//		kb.deposit(10000, gildong);
+//		System.out.println("내 돈 : " +gildong.personMoney);
+//		System.out.println("은행 돈 : " + kb.bankMoney);
+		
+		kb.withdraw(10000, gildong);
+		System.out.println("내 돈 : " +gildong.personMoney);
+		System.out.println("은행 돈 : " +kb.bankMoney);
 		
 	}
 }
--- practice/src/practiceMethod/PracticeMethod.java
+++ practice/src/practiceMethod/PracticeMethod.java
@@ -1,7 +1,192 @@
 package practiceMethod;
 
+import java.util.Scanner;
+
+import practiceMethod.PracticeMethod;
+
 public class PracticeMethod {
+//      1. 두 정수를 전달하면 몫과 나머지를 배열로 전달하는 메서드
+//	int[] divide(int dividend, int divisor) {
+//        if (divisor == 0) {
+//            System.out.println("0으로 나눌수 없습니다.");
+//        }
+//        
+//        int quotient = dividend / divisor; 
+//        int remainder = dividend % divisor; 
+//
+//        return new int[]{quotient, remainder};
+//	}	
+	
+//	int[] getDivide(int data1, int data2) {
+//	      return new int[] {data1 / data2, data1 % data2};
+//	   };
+//	
+//	2. 문자열에서 숫자만 추출하는 메서드
+	
+//	String integerValue(String inputText) {
+//		if (inputText == null) {
+//            return "";
+//        }
+//
+//        String result = "";
+//        for (int i = 0; i < inputText.length(); i++) {
+//            char ch = inputText.charAt(i);
+//
+//            if (ch >= '0' && ch <= '9') {
+//                result += ch; 
+//            }
+//        }
+//        return result;
+//    }
+//	
+//	
+//	 String getIntegerString(String input) {
+//	      String result = "";
+//	      for(int i = 0; i < input.length(); i++) {
+//	         int c = input.charAt(i) - 48;
+//	         if(c >= 1 && c <= 9) {
+//	            result += (char)(c + 48);
+//	         }
+//	      }
+//	      return result;
+//	   }
+//
+//
+//	3. 문자열에서 특정 문자의 개수를 세는 메서드
+//	
+//	int getCount(String expression, char c) {
+//		int count = 0;
+//		for(int i =0; i< expression.length(); i++) {
+//			if(expression.charAt(i) == c) {
+//				count++;
+//			}
+//		}
+//		return count;
+//	}
+//	
+//	
+//	4. 배열에서 홀수만 합산하는 메서드
+//	
+//	 int getTotalOdd(int[] datas) {
+//	      int result = 0;
+//	      for(int i = 0; i < datas.length; i++) {
+//	         if(datas[i] % 2 == 1) {
+//	            result += datas[i];
+//	         }
+//	      }
+//	      return result;
+//	   }
+//	   
+//	 5. 배열에서 가장 작은 두 수를 찾아 반환하는 메서드
+//	
+//	 int[] getMinArray(int[] datas) {
+//	      int[] minArray = new int[2];
+//	      int min1 = datas[0], min2 = datas[1];
+//	      for(int i = 0; i < datas.length; i++) {
+//	         if(min1 > datas[i]) {
+//	            min1 = datas[i];
+//	         }else if(datas[i] > min1 && min2 >= datas[i]){
+//	            min2 = datas[i];
+//	         }
+//	      }
+//	      minArray[0] = min1;
+//	      minArray[1] = min2;
+//	      return minArray;
+//	   }
+//	
+//	
+//	 6. 문자열에서 중복 문자를 제거하는 메서드
+//	
+	String getRemoveString(String input) {
+		String result = "";
+		for(int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if(!result.contains(String.valueOf(c))) {
+				result += c;
+			}
+		}
+		return result;
+	}
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-		
+		PracticeMethod pmt = new PracticeMethod();
+	    Scanner sc = new Scanner(System.in);
+//	    System.out.println("정수 두개를 입력하세요.");
+//	    int value1 = sc.nextInt();
+//	    int value2 = sc.nextInt();
+//	    int[] result = pmt.divide(value1, value2);
+//      System.out.println("몫: " + result[0]);
+//      System.out.println("나머지: " + result[1]);
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+//      2. 사용자가 입력하는 값중 정수 분리하기
+//      입력 예시)
+//      안1녕2하3세4요
+//      출력 예시)
+//      1234
+//	    String[] value = null;
+//	    String inputText = "", message = "글자 사이에 숫자를 찾아서 숫자만 작성합니다.\nex)안1녕2하3세4요 -> 1234";
+//	    System.out.println(message);
+//	    inputText = sc.next();
+//	    String result = pmt.integerValue(inputText);
+//
+//      System.out.println("입력: " + inputText);
+//      System.out.println("숫자만 추출: " + result);
+//	   
+	   
+//     3. 사용자가 입력한 글자와 찾을 글자를 입력하면 글자의 개수를 반환해주는 메서드
+//     입력 예시) apple
+//     찾을 글자 p
+//     반환 2
+	   
+//      String input = null;
+//      input = sc.next();
+//      int count = pmt.getCount(input, 'a');
+//      System.out.println(count);
+       
+//     4. 5개 정수를 배열로 마구잡이로 전달할 때 가장 작은 2개의 숫자를 배열에 담아 전달하는 메서드
+
+//     int[] datas = {10, 40, 1, 10};
+//     System.out.println(pmt.getMinArray(datas)[0]);
+//     System.out.println(pmt.getMinArray(datas)[1]);
+     
+       
+//      5. 랜덤한 값을 100칸 배열에 담은 후 배열 안의 값 중 홀수의 합만 출력
+//     int[] datas = new int[100];
+//     int result = 0;
+//     for(int i = 0; i < datas.length; i++) {
+//        datas[i] = (int)(Math.random() * 100);
+//     }
+//     
+//     System.out.println(pmt.getTotalOdd(datas));
+      
+//     6. 연속된 중복 문자 제거하기
+//     입력 예시1)
+//     abbbbacda
+//     출력 예시1)
+//     aacda
+      
+//     입력 예시2)
+//     aabbccccd
+//     출력 예시2)
+//     abcd
+	
+//	   Scanner sc = new Scanner(System.in);
+       String input = null, message = "중복을 제거할 문자를 입력하세요.\naabbccccd", result = "";
+	      
+       System.out.println(message);
+       input = sc.next();
+	      
+       System.out.println(pmt.getRemoveString(input));
 	}
 }
